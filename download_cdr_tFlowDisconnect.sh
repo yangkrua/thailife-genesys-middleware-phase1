ea6861201b704c7ecf4a9e80f895c2042c151868
GMSomsak <gmsomsak@gmail.com>
2024-11-12 17:11:25 +0700

    create new shell file

---
+++ download_cdr_tFlowDisconnect.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /root/middleware/genesys_download_cdr
+/usr/node-v20.11.0-linux-x64/bin/node download_cdr_tFlowDisconnect.js
---
+++ getA_NumberReport.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /root/middleware/genesys_download_cdr
+/usr/node-v20.11.0-linux-x64/bin/node getA_NumberReport.js
